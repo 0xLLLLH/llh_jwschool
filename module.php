5f4aac54502593d243e4c3f5a4c5e5ad30c9cdc4
0xLLLLH <linlihao159@gmail.com>
2016-03-26 14:22:04 +0800

    基本文件
    基本文件

---
+++ module.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 简玩校园模块定义
+ *
+ * @author 0xLLLLH
+ * @url http://bbs.we7.cc/
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class XLLLLH_jwschoolModule extends WeModule {
+
+
+}
---
+++ processor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 简玩校园模块处理程序
+ *
+ * @author 0xLLLLH
+ * @url http://bbs.we7.cc/
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class XLLLLH_jwschoolModuleProcessor extends WeModuleProcessor {
+	public function respond() {
+		$content = $this->message['content'];
+		//这里定义此模块进行消息处理时的具体过程, 请查看微擎文档来编写你的代码
+	}
+}
