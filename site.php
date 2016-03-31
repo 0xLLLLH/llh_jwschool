286a5a4656a41a23ab1e5be3a797e67a641f415f
0xLLLLH <linlihao159@gmail.com>
2016-03-31 22:00:04 +0800

    修改目录结构
    修改目录结构

--- site.php
+++ site.php
@@ -9,15 +9,5 @@
 defined('IN_IA') or exit('Access Denied');
 
 class llh_jwschoolModuleSite extends WeModuleSite {
-    public function doMobileIndex() {
-        //这个操作被定义用来呈现 功能封面
-        global  $_W,$_GPC;
-        include $this->template('index');
-    }
 
-    public function doMobileWeUsercenter() {
-        //这个操作被定义用来呈现 微站个人中心导航
-        global  $_W,$_GPC;
-        include $this->template('usercenter');
-    }
 }
