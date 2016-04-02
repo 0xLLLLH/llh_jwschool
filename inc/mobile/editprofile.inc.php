646cc539fe27f3bea7d2c4e83cd5bf69c142f3d0
ZT837606704 <837606704@qq.com>
2016-04-02 13:05:42 +0800

    个人信息获取和更新

--- inc/mobile/editprofile.inc.php
+++ inc/mobile/editprofile.inc.php
@@ -6,6 +6,31 @@
  * Description：个人详细资料编辑
  */
 
-global  $_W,$_GPC;
+global $_W, $_GPC;
 load()->func('tpl');
+load()->model('mc');
+
+if (isset($_GPC['submit'])) {
+    $field=$_GPC['op'];//更新的字段
+    mc_update(intval($_W['member']['uid']),array($field=>$_GPC[$field]));
+    unset($_GPC['op']);//重置操作
+}
+/**
+ * 信息填充
+ */
+if (!empty($_W['member']['uid'])) {
+    $member = mc_fetch(intval($_W['member']['uid']), array('nickname','realname','gender','mobile','email','qq','birthyear','birthmonth','birthday','resideprovince','residecity','residedist'));
+}
+
+$member['birth'] = array(
+    'year'  => $member['birthyear'],
+    'month' => $member['birthmonth'],
+    'day'   => $member['birthday']
+);
+$member['reside'] = array(
+    'province' => $member['resideprovince'],
+    'city'     => $member['residecity'],
+    'district' => $member['residedist']
+);
+
 include $this->template('editprofile');
--- inc/mobile/index.inc.php
+++ inc/mobile/index.inc.php
@@ -7,53 +7,7 @@
  */
 global $_W, $_GPC;
 
-//echo $_W['openid'];
 
-load()->model('mc');
-$avatar = '';
-/*
-var_dump($_W['member']);
-echo "<br>-----------<br>";
-var_dump($_W['fans']);*/
-echo intval($_W['member']['uid']);
-echo mc_update(intval($_W['member']['uid']), array('realname' => 'zhangtao'));
-var_dump(mc_fetch(15,array('nickname','realname','mobile')));
-
-if (!empty($_W['member']['uid'])) {
-    $member = mc_fetch(intval($_W['member']['uid']), array('avatar'));//获取uid的avatar字段
-    //var_dump($member);
-    if (!empty($member)) {
-        $avatar = $member['avatar'];
-    }
-}
-if (empty($avatar)) {
-    $fan = mc_fansinfo($_W['openid']); //获取粉丝信息
-    if (!empty($fan)) {
-        $avatar = $fan['avatar'];
-    }
-}
-if (empty($avatar)) {
-    $userinfo = mc_oauth_userinfo();//调用oauth用户授权获取资料并更新会员信息
-    //var_dump($userinfo);
-    if (!is_error($userinfo) && !empty($userinfo) && is_array($userinfo) && !empty($userinfo['avatar'])) {
-        $avatar = $userinfo['avatar'];
-        $nickname = $userinfo['nickname'];
-    }
-}
-/*
-if (empty($avatar) && !empty($_W['member']['uid'])) {
-    $avatar = mc_require($_W['member']['uid'], array('avatar'));//显示表单让用户填写
-}*/
-if (empty($avatar)) {
-    // 提示用户关注公众号。;
-    //echo "最终没有获取到头像,follow: {$_W['fans']['follow']}";
-} else {
-
-      echo <<<IMG
-  <img src="$avatar">
-IMG;
-}
-global  $_W,$_GPC;
 $url=$this->createMobileUrl('usercenter');
 
 $pagelist=array('个人中心'=>$url);
