5a541039ba2956fa6626889a561d45d24da9eac7
0xLLLLH <linlihao159@gmail.com>
2016-04-02 20:42:20 +0800

    newstate.html

---
+++ inc/mobile/newstate.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Phpstorm.
+ * User: 0xLLLLH
+ * Date: 2016.4.2
+ * Description：发布新状态
+ */
+
+global  $_W,$_GPC;
+load()->func('tpl');
+
+include $this->template('newstate');
