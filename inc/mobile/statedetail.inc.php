9428f5b8eadbcde208b3b1cebe82a2780851705f
0xLLLLH <linlihao159@gmail.com>
2016-04-04 20:10:31 +0800

    约游详情页前端

---
+++ inc/mobile/statedetail.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Phpstorm.
+ * User: 0xLLLLH
+ * Date: 2016.4.3
+ * Description：状态详情
+ */
+
+global  $_W,$_GPC;
+load()->func('tpl');
+include $this->template('statedetail');
